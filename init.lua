88f81f09a9e1a0d3e2bae2229d9ff93e906eebaf
sylvain hellin <hellinsylvain@gmail.com>
2025-11-18 21:49:54 +0100

    added checkpoint for reading the init.lua

--- init.lua
+++ init.lua
@@ -44,6 +44,7 @@ What is Kickstart?
 Kickstart Guide:
 
   TODO: The very first thing you should do is to run the command `:Tutor` in Neovim.
+  -- ongoing
 
     If you don't know what this means, type the following:
       - <escape key>
